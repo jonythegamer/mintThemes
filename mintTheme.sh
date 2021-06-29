5f61ed5b6f8d8fe17bcd8f0c8cab41cc89f060a3
jonythegamer <55815482+jonythegamer@users.noreply.github.com>
2021-06-29 17:57:39 -0400

    Add files via upload

---
+++ mintTheme.sh
@@ -0,0 +1,14 @@
+cd ~/Downloads
+#grab the icon repositros and move them into the themes folder
+git clone https://github.com/linuxmint/mint-themes.git
+git clone https://github.com/linuxmint/mint-y-icons.git
+sudo mv mint-themes/files/usr/share/themes/ /usr/share/themes/
+sudo mv mint-y-icons/usr/share/icons/Mint-Y /usr/share/icons/
+#Grab the background and start menu icon repository
+git clone https://github.com/jonythegamer/Linux-mint-backgrounds.git
+cd Linux-mint-backgrounds/Linux\ mint\ themes
+sudo cp -r linuxmint* /usr/share/backgrounds
+cd ~/Pictures
+mkdir Icons && cd Icons
+mkdir Linux\ mint && cd Linux\ mint
+mv ~/Downloads/Linux-mint-backgrounds/Linux\ mint\ themes/ Start\ menu\ icon.png Start\ menu\ icon.png
