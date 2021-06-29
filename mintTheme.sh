43de64f5fd905dbad3030f3e2bea583beaa31f53
jonythegamer <55815482+jonythegamer@users.noreply.github.com>
2021-06-29 18:17:13 -0400

    Update mintTheme.sh

--- mintTheme.sh
+++ mintTheme.sh
@@ -1,9 +1,14 @@
-cd ~/Downloads
-#grab the icon repositros and move them into the themes folder
+#Creating some directories
+cd ~/Pictures
+mkdir backgrounds && cd backgrounds
+mkdir linuxmint && cd ~/Downloads
+
+#Grab the icon repositros and move them into the themes folder
 git clone https://github.com/linuxmint/mint-themes.git
 git clone https://github.com/linuxmint/mint-y-icons.git
 sudo mv mint-themes/files/usr/share/themes/ /usr/share/themes/
 sudo mv mint-y-icons/usr/share/icons/Mint-Y /usr/share/icons/
+
 #Grab the background and start menu icon repository
 git clone https://github.com/jonythegamer/Linux-mint-backgrounds.git
 cd Linux-mint-backgrounds/Linux\ mint\ themes
